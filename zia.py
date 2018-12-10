5c6fea3bdbcb59ea9755755e3e70a31235baf569
uzbit <uzbit@uzbit.net>
2018-12-10 20:33:06 +0700

    Work on basic zia fractal

--- zia.py
+++ zia.py
@@ -5,12 +5,15 @@ import numpy as np
 
 class Zia(object):
 
-    def __init__(self, radius, rayLen, thickness, N=500):
+    def __init__(self, radius, rayLen, scale, thickness=100, rayN=500, sunN=500):
         self._r = radius
         self._l = rayLen
+        self._s = scale
         self._t = thickness
         self._d = 2.0*self._r/6.0
-        self._N = N
+        self._rN = rayN
+        self._sN = sunN
+
 
     def genZia(self):
         xpts, ypts = np.array([]), np.array([])
@@ -21,7 +24,7 @@ class Zia(object):
         xpts = np.append(xpts, ptsx)
         ypts = np.append(ypts, ptsy)
 
-        return np.array(xpts), np.array(ypts)
+        return self._s * np.array(xpts), self._s * np.array(ypts)
 
     def genNorthRays(self):
         rays = list()
@@ -35,10 +38,11 @@ class Zia(object):
             )
 
         xpts, ypts = list(), list()
-        for ray in rays:
-            dx = (ray[1][0] - ray[0][0])/float(self._N)
-            dy = (ray[1][1] - ray[0][1])/float(self._N)
-            for i in range(self._N):
+        for k, ray in enumerate(rays):
+            N = self._rN + (1 if k in [1,2] else 0)
+            dx = (ray[1][0] - ray[0][0])/float(N)
+            dy = (ray[1][1] - ray[0][1])/float(N)
+            for i in range(N):
                 xpts.append(ray[0][0] + dx * i)
                 ypts.append(ray[0][1] + dy * i)
 
@@ -61,7 +65,8 @@ class Zia(object):
 
     def genSun(self):
         xpts, ypts = list(), list()
-        for t in np.linspace(0, 2 * np.pi, self._N):
+        #for t in np.linspace(0, 2 * np.pi, self._sN):
+        for t in [np.pi/4.0, 3*np.pi/4.0, 5*np.pi/4.0, 7*np.pi/4.0]:
             xpts.append(self._r * np.cos(t))
             ypts.append(self._r * np.sin(t))
 
@@ -71,12 +76,14 @@ class Zia(object):
         fig, ax = plt.subplots()
         xpts, ypts = self.genZia()
         ax.set_aspect('equal')
+
+        #plt.axis('off')
         plt.scatter(xpts, ypts, s=self._t, color='r')
         plt.savefig('zia.png')
         plt.show()
 
 def main():
-    zia = Zia(1, 2, 100)
+    zia = Zia(1, 2, 1, thickness=100)
     zia.draw()
 
 
--- ziafract.py
+++ ziafract.py
@@ -1,1 +1,37 @@
 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+from zia import Zia
+
+def fract(xpts, ypts, scale, rot):
+    if scale <= 0.001:
+        return xpts, ypts
+    newxpts, newypts = list(), list()
+    for xpt, ypt in zip(xpts, ypts):
+        xpts1, ypts1 = list((xpts*scale + xpt)), list((ypts*scale + ypt))
+        newxpts.append(xpts1)
+        newypts.append(ypts1)
+    return fract(np.array(newxpts).flatten(), np.array(newypts).flatten(), 0.01*scale, rot)
+
+def main():
+    ziaObj = Zia(1.0, 2.0, 100, thickness=100, rayN=5, sunN=4)
+    xpts, ypts = ziaObj.genZia()
+    xpts, ypts = fract(xpts, ypts, 0.045, 10)
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+
+    #plt.axis('off')
+    matplotlib.rcParams['markers.fillstyle'] = 'full'
+    matplotlib.rcParams['scatter.marker'] = ','
+    matplotlib.rcParams['lines.markersize'] = 1
+
+    plt.scatter(xpts, ypts, color='black')
+    plt.savefig('ziafract.pdf')
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
