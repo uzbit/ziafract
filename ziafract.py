971c1d1058da8004055fd75d540bfefb2fe84043
uzbit <uzbit@uzbit.net>
2018-12-17 14:03:06 +0700

    Cleanup ziafract, work on fractal

--- ziafract.py
+++ ziafract.py
@@ -1,33 +1,46 @@
-
 import matplotlib
-matplotlib.use('macosx')
 import matplotlib.pyplot as plt
 import numpy as np
 from zia import Zia
 
-def fract(xpts, ypts, scale, rot):
-    if scale <= 0.001:
+NUM_DEPTH = 2
+SCALE_STEPDOWN = 0.01
+INIT_SCALE = 0.05
+SCALE_DEPTH = INIT_SCALE * np.power(SCALE_STEPDOWN, NUM_DEPTH)
+MAX_PLT_PTS = 1000000
+
+def fract(xpts, ypts, scale):
+    print("Calculating for scale = %f, depth = %f, npts = %d..." % (scale, SCALE_DEPTH, len(xpts)))
+    if scale <= SCALE_DEPTH:
+        print("Reached max depth.")
         return xpts, ypts
     newxpts, newypts = list(), list()
     for xpt, ypt in zip(xpts, ypts):
-        xpts1, ypts1 = list((xpts*scale + xpt)), list((ypts*scale + ypt))
+        xpts1, ypts1 = (xpts*scale + xpt), (ypts*scale + ypt)
         newxpts.append(xpts1)
         newypts.append(ypts1)
-    return fract(np.array(newxpts).flatten(), np.array(newypts).flatten(), 0.01*scale, rot)
+    return fract(np.array(newxpts).flatten(), np.array(newypts).flatten(), SCALE_STEPDOWN*scale)
+
+    return tf.dynspread(img, threshold=0.50, name=name)
 
 def main():
     ziaObj = Zia(1.0, 2.0, 1, rayN=5, sunN=4)
     xpts, ypts = ziaObj.genZia()
-    xpts, ypts = fract(xpts, ypts, 0.05, 10)
+    xpts, ypts = fract(xpts, ypts, INIT_SCALE)
+    print("Finished calculating %d fractal points." % (len(xpts)))
+
     fig, ax = plt.subplots()
     ax.set_aspect('equal')
-
     plt.axis('off')
     matplotlib.rcParams['markers.fillstyle'] = 'full'
     matplotlib.rcParams['scatter.marker'] = '.'
     matplotlib.rcParams['lines.markersize'] = 1
+    if len(xpts) > MAX_PLT_PTS:
+        subinds = np.random.choice(len(xpts), size=MAX_PLT_PTS, replace=False)
+    else:
+        subinds = list(range(len(xpts)))
 
-    plt.scatter(xpts, ypts, s=1, color='black')
+    plt.scatter(xpts[subinds], ypts[subinds], s=1, color='black')
     plt.savefig('ziafract.pdf')
     plt.show()
 
