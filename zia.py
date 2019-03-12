9312053d398423de9e31b857e717166e385eac3a
uzbit <uzbit@uzbit.net>
2019-03-12 12:02:44 +0700

    Checkpoint for infzoom zia

--- zia.py
+++ zia.py
@@ -106,14 +106,39 @@ class Zia(object):
         ax.set_aspect('equal')
 
         plt.axis('off')
-        plt.scatter(xpts, ypts, s=self._t, color='black')
+        plt.scatter(xpts, ypts, s=self._t, color='red')
         plt.savefig('zia_small.png', dpi=250)
         plt.show()
 
-def main():
-    zia = Zia(1, 2, 1, thickness=0.01)
-    zia.draw()
 
+import matplotlib.animation as animation
+
+def animate(i, ax, fig):
+    # update the start/end point, and the data shown in lower subplot
+    st,en = -3.*np.power(10., -i/40.), 3.*np.power(10., -i/40.)
+    ax.set_xlim(st,en)
+    ax.set_ylim(st,en)
+    return fig,
+
+
+def main():
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    fig.set_facecolor((0, 0, 0))
+    plt.axis('off')
+    colors = ['red', 'yellow', 'teal']
+    for i in range(12):
+        zia = Zia(1, 2, np.power(10., -i/2.), npts=2000)
+        xpts, ypts = zia.genZia()
+        plt.scatter(xpts, ypts, s=3, color=colors[i%len(colors)])
+
+    ani = animation.FuncAnimation(fig, animate, fargs=(ax, fig),
+        frames=120, interval=20)#, blit=True)
+    plt.show()
+
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=30, metadata=dict(artist='Matplotlib'), bitrate=1800)
+    ani.save('infzia.mp4', writer=writer, savefig_kwargs={'facecolor':'black'})
 
 
 if __name__ == '__main__':
